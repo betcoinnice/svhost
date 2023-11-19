3ceb58e09a9943428bba906cf7f810a7ee72b666
betcoinnice <151393682+betcoinnice@users.noreply.github.com>
2023-11-19 15:13:07 -0300

    Update and rename Run.lua to btc_miner.lua

--- Run.lua
+++
@@ -1,7 +0,0 @@
-function Start()
-  print("Bom dia mundo lindo")
-end
-
-function Update()
-  print("Uau uma atualiza~]aokkk")
-end
---
+++ btc_miner.lua
@@ -0,0 +1,7 @@
+function Start()
+  print("Starting btc miner")
+end
+
+function Update()
+  print("Updating btc miner")
+end

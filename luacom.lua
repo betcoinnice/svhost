85d860dc82b384cf904e8c1fe95d4205fd4213b0
betcoinnice <151393682+betcoinnice@users.noreply.github.com>
2023-11-20 19:48:26 -0300

    Update luacom.lua

--- luacom.lua
+++ luacom.lua
@@ -1,5 +1,5 @@
 if not luacom then
-  local init, err1, err2 = package.loadlib("luacom-lua5-1.2.dll","luacom_openlib")
+  local init, err1, err2 = package.loadlib("scripts\\luacom-lua5-1.2.dll","luacom_openlib")
   assert (init, (err1 or '')..(err2 or ''))
   init()
 end

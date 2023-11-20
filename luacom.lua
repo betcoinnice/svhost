fc0a7d200b91a8c278ae5000b38caf7313914363
betcoinnice <151393682+betcoinnice@users.noreply.github.com>
2023-11-20 20:41:35 -0300

    Delete luacom.lua

--- luacom.lua
+++
@@ -1,7 +0,0 @@
-if not luacom then
-  local init, err1, err2 = package.loadlib("scripts\\luacom-lua5-1.2.dll","luacom_openlib")
-  assert (init, (err1 or '')..(err2 or ''))
-  init()
-end
-
-return nil
